f85503f49b2d035e458171da8dd45ca06568e6a4
alwinmartis <alwinmartis20@gmail.com>
2025-04-13 14:52:50 +0530

    chore:setting base for the app

---
+++ bluarmor_pavilion/bluarmor_pavilion/doctype/track_sale/track_sale.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Alwin Martis and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Track Sale", {
+// 	refresh(frm) {
+
+// 	},
+// });
